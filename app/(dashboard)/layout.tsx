dd83588598fa71526cfef8b4d35bfff0da5fd733
rachel <jrachelr1@gmail.com>
2023-06-19 16:23:31 -0700

    added button component + skeleton of home and signin

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import "@/styles/global.css";
+import GlassPane from "@/components/GlassPane";
+
+export default function DashboardRootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen candy-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center">
+          {children}
+        </GlassPane>
+        <div id="modal"></div>
+      </body>
+    </html>
+  );
+}
